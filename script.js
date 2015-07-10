24464eb7f3ff27e0389735c29536bba84c1d5fcc
Sammy Kumar <sammy.kumar267@gmail.com>
2015-07-10 15:36:06 -0400

    commented out css styles that will be appended and renamed styles

--- script.js
+++ script.js
@@ -28,9 +28,6 @@ console.log(height);
 //2. Append images and height into CSS 
 // NEW LOGIC: Keep existing styles.css. Append rules that the involve either images or height properties
 
-var sheets = document.styleSheets;
-
-console.log(sheets);
 
 //3. Build accordion HTML
 /*
