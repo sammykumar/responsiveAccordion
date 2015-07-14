91b0864941e6a9e808fe4589be868340f9c5a566
Adam Schwartz <aschwartz@hubspot.com>
2015-07-14 15:16:17 -0400

    Updates

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function(){
   // Our app needs modern browser support
   if (!document.addEventListener && !document.documentElement.classList && !document.documentElement.getAttribute) return;
- 
+
   var ready = function(fn) {
     if (document.readyState !== 'loading') {
       fn();
@@ -9,17 +9,17 @@
       document.addEventListener('DOMContentLoaded', fn);
     }
   };
-  
+
   var options = INSTALL_OPTIONS;
-  
+
   var styleEl = document.createElement('style');
   var styleString = '.eager-accordion { height:' + options.height + 'px }\n';
   for (var i = 0; i < options.images.length; i++) {
-    styleString += '.eager-accordion ul li:nth-child(' + (i + 1) + ') { background-image: url("' + images[i] + '") }\n';
+    styleString += '.eager-accordion ul li:nth-child(' + (i + 1) + ') { background-image: url("' + options.images[i].url + '") }\n';
   }
   styleEl.innerHTML = styleString;
   document.body.appendChild(styleEl);
- 
+
   var buildAccordian = function(images) {
     /*
     <div class="eager-accordion">
@@ -36,37 +36,40 @@
           ...
         </li>
       </ul>
-    </div>		
+    </div>
     */
-    
+
     var div = document.createElement('div');
     div.classList.add('eager-accordion');
-    
-	var ul = document.createElement('ul');
-		
-	for (var i = 0; i < images.length; i ++) {
-	  var li = document.createElement('li'),
-	      liDiv = document.createElement('div'),
-          anchor = document.createElement('a'),
-	      h2 = document.createElement('h2'),
-	      p = document.createElement('p');
+
+    var ul = document.createElement('ul');
+
+    for (var i = 0; i < images.length; i ++) {
+      var li = document.createElement('li'),
+        liDiv = document.createElement('div'),
+        anchor = document.createElement('a'),
+        h2 = document.createElement('h2'),
+        p = document.createElement('p');
 
       p.appendChild(document.createTextNode(images[i].caption));
       h2.appendChild(document.createTextNode(images[i].title));
-      anchor.setAttribute('href', images[i].linkHref);
-		
+
+      if (images[i].linkHref) {
+        anchor.setAttribute('href', images[i].linkHref);
+      }
+
       anchor.appendChild(h2);
       anchor.appendChild(p);
       liDiv.appendChild(anchor);
       li.appendChild(liDiv);
       ul.appendChild(li);
-	}
+    }
 
-	div.appendChild(ul);
-	return div;
+    div.appendChild(ul);
+    return div;
   }
-  
+
   ready(function(){
-    Eager.createElement(options.location).appendChild(buildAccordian(options.images))
+    Eager.createElement(options.location).appendChild(buildAccordian(options.images));
   });
 })();
